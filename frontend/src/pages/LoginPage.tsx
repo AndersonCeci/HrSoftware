ccd0fc642cb448e3826d68f76bb52ebd5e6367bb
nkreshpaj <nikolkreshpaj@gmail.com>
2024-07-11 14:44:21 +0200

    loginform

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,12 +1,56 @@
-import { NavLink } from "react-router-dom"
+import { Button, Form, Input } from "antd";
+import { LockOutlined, UserOutlined } from "@ant-design/icons";
+import "../styles/LoginPage.css";
+import NavigationMenuLogo from "../components/Navigation/NavigationMenuLogo";
 
 const LoginPAge: React.FC = () => {
-    return (
-        <div>
-            <h1>Login Page</h1>
-            <NavLink to={"/your-info/dashboard"}>DASHBOARD</NavLink>
-        </div>
-    )
-}
+  const onFinish = (values: unknown) => {
+    console.log("Received values of form:", values);
+  };
 
-export default LoginPAge
+  return (
+    <div className="login-page">
+      <Form
+        name="normal-login"
+        className="login-form"
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+      >
+        <NavigationMenuLogo logotext={"HRSoftware"} />
+        <h2 className="login-title">Login</h2>
+        <Form.Item
+          name="username"
+          className="username-input"
+          rules={[{ required: true, message: "Please input your username" }]}
+        >
+          <Input
+            prefix={<UserOutlined className="site-form-item-icon" />}
+            placeholder="Username"
+          />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          className="password-input"
+          rules={[{ required: true, message: "Please input your password" }]}
+        >
+          <Input
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Item>
+        <Form.Item>
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+          >
+            Log in
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
+
+export default LoginPAge;
