089ce62a2be3ad89b492029f9393ab8bb6d20d19
IlviCumani <ilviCumani@gmail.com>
2024-07-16 10:46:48 +0200

    Added a modal component and fixed a bug with table

--- frontend/src/components/Table/TableHeader.tsx
+++ frontend/src/components/Table/TableHeader.tsx
@@ -3,11 +3,11 @@ import { PlusCircleOutlined } from "@ant-design/icons";
 import Button from "../Shared/Button";
 import { ButtonSize } from "../../enums/Button";
 
-const TableHeader = ({ title }: { title: string }) => {
+const TableHeader = ({ title, onClick }: { title: string, onClick?: () => void | undefined }) => {
 	return (
 		<Flex align="center" justify="space-between">
 			<h1>{title}</h1>
-			<Button icon={<PlusCircleOutlined />} size={ButtonSize.LARGE}>
+			<Button icon={<PlusCircleOutlined />} size={ButtonSize.LARGE} onClick={onClick}>
 				Add New
 			</Button>
 		</Flex>
