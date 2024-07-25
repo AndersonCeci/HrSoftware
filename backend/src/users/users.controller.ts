bf353df204d08cc23383c374dfc67f8f4e304337
Gerald Bane <geraldbane3@gmail.com>
2024-07-25 14:38:48 +0200

    update password

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,27 +1,34 @@
-import { Controller, Post, UsePipes, Body, ValidationPipe, Get, Param } from "@nestjs/common";
+import { Controller, Post, Body, Get, Param, Put, Req, UseGuards } from "@nestjs/common";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { UserService } from "./users.service";
+import { ChangePasswordDTO } from "./dto/update-password.dto";
+import { AuthGuard } from "src/auth/guards/auth.guard";
 
 @Controller('users')
 export class UsersController {
-
     constructor(private userService: UserService) { }
 
     @Post()
-    @UsePipes(new ValidationPipe())
     async createUser(@Body() createUserDto: CreateUserDto) {
-        return this.userService.createUser(createUserDto);
+        return await this.userService.createUser(createUserDto);
     }
 
     @Get()
-    getUsers() {
-        return this.userService.getUsers();
+    async getUsers() {
+        return await this.userService.getUsers();
+    }
+
+    @Get(':username')
+    async getUserByUsername(@Param('username') username: string) {
+        return await this.userService.getUserByUsername(username);
     }
     
-
-    @Get(':id')
-    async getUserById(@Param('id') id: string) {    
-
+    @UseGuards(AuthGuard)
+    @Put('change-password')
+    async updatePassword(
+        @Req() req: any,
+        @Body() updatePasswordDto: ChangePasswordDTO
+    ) {
+        return await this.userService.updatePassword(req.user.username,updatePasswordDto.oldPassword, updatePasswordDto.newPassword);
     }
-
 }
--- frontend/src/pages/Settings/SettingsPage.tsx
+++ frontend/src/pages/Settings/SettingsPage.tsx
@@ -1,9 +1,10 @@
+import ChangePasswordForm from "./components/ChangePasswordForm"
+import { PasswordProvider } from "./context"
 
 const SettingsPage : React.FC = () =>{
   return (
     <div>
-      
-     
+      <PasswordProvider children={<ChangePasswordForm/>}/>
     </div>
   )
 }
