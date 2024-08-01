2533051a673d04bec8f2342593bbb5a6967147a6
IlviCumani <ilviCumani@gmail.com>
2024-08-01 16:47:04 +0200

    fixed recruitment form

--- frontend/src/components/Table/TableHeader.tsx
+++ frontend/src/components/Table/TableHeader.tsx
@@ -3,18 +3,28 @@ import { PlusCircleOutlined } from "@ant-design/icons";
 import Button from "../Shared/Button";
 import { ButtonSize, ButtonType } from "../../enums/Button";
 
-const TableHeader = ({ title, onClick }: { title: string; onClick?: () => void | undefined }) => {
+const TableHeader = ({
+	title,
+	onClick,
+	hideButton,
+}: {
+	title: string;
+	onClick?: () => void | undefined;
+	hideButton?: boolean;
+}) => {
 	return (
 		<Flex justify="space-between" align="center">
 			<Typography.Title>{title}</Typography.Title>
-			<Button
-				icon={<PlusCircleOutlined />}
-				size={ButtonSize.LARGE}
-				type={ButtonType.PRIMARY}
-				onClick={onClick}
-			>
-				Add {title}
-			</Button>
+			{!hideButton && (
+				<Button
+					icon={<PlusCircleOutlined />}
+					size={ButtonSize.LARGE}
+					type={ButtonType.PRIMARY}
+					onClick={onClick}
+				>
+					Add {title}
+				</Button>
+			)}
 		</Flex>
 	);
 };
