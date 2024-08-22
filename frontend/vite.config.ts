89dc80f3ec6f158a2d3160e94fd5ed0968d41d91
nkreshpaj <nikolkreshpaj@gmail.com>
2024-08-22 14:48:37 +0200

    fixproxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,28 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const userData = import.meta.env.REACT_APP_EMPLOYEE_API;
-
 export default defineConfig({
   plugins: [react()],
   envPrefix: "REACT_APP_",
   server: {
-    proxy:
-      userData.role === "hr"
-        ? {
-            "/api/today": {
-              target: "https://zenquotes.io",
-              changeOrigin: true,
-              rewrite: (path) => path.replace(/^\/api\/today/, "/api/today"),
-            },
-          }
-        : {
-            "/api/joke": {
-              target: "https://v2.jokeapi.dev",
-              changeOrigin: true,
-              rewrite: (path) =>
-                path.replace(/^\/api\/joke/, "/joke/Programming?type=single"),
-            },
-          },
+    proxy: {
+      "/api/today": {
+        target: "https://zenquotes.io",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/today/, "/api/today"),
+      },
+
+      "/api/joke": {
+        target: "https://v2.jokeapi.dev",
+        changeOrigin: true,
+        rewrite: (path) =>
+          path.replace(/^\/api\/joke/, "/joke/Programming?type=single"),
+      },
+    },
   },
 });
